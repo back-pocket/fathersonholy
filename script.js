018869c073c3646053cf49e773b19d11fc0a458b
back-pocket <chloegillen141@gmail.com>
2025-05-15 17:18:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function redirect() {
 
   switch (selectedValue) { 
       case "option1": 
-        document.location = "https://github.com/back-pocket/fathersonholy/blob/3873b257e6985d46ee67edd202cf7b48a17d2f4c/son.html; 
+        document.location = "https://github.com/back-pocket/fathersonholy/blob/3873b257e6985d46ee67edd202cf7b48a17d2f4c/son.html"; 
         alert("Allow Surviellance of Yr Body and Mind.") 
         break; 
       case "option2": 
-          document.location = "father.html"; 
+          document.location = "https://github.com/back-pocket/fathersonholy/blob/a5245a234ba665f51c4ef72e3b8d92447a3a8bdb/father.htmll"; 
         alert("Allow Surviellance of Yr Body and Mind.") 
           break; 
       case "option3": 
